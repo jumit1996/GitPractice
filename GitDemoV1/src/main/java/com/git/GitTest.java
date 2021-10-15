c83545231f6fcc5d1473848ea61b595ddbc07479
hp <hp@LAPTOP-9MD3T8QU>
2021-10-15 13:24:20 +0530

    Commit15

---
+++ GitDemoV1/src/main/java/com/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class GitTest {
+public static void main(String[] args) {
+	System.out.println("Git practice start");
+	
+	
+	System.out.println("Git practice end");
+}
+}
